c6a6653dbc0f2b1c5a475e8fbd818c7774379a59
Chandan8585 <chandanwingshr@gmail.com>
2023-10-14 21:21:27 +0530

    Signup page UI created

--- chat_app/src/Pages/home/HomePage.jsx
+++ chat_app/src/Pages/home/HomePage.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import "./Home.css"
+import Login from '../../components/auth/Login';
+
+const initalValues = {
+    email: "",
+    password: "" 
+}
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+        <Login/>
+    </div>
   )
 }
 
 export default HomePage
+
