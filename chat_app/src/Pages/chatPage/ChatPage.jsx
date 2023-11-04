083c688d8fef960cfcc432cd3253bec6cd5684d5
Chandan8585 <chandanwingshr@gmail.com>
2023-11-04 23:05:42 +0530

    chat page creating

--- chat_app/src/Pages/chatPage/ChatPage.jsx
+++ chat_app/src/Pages/chatPage/ChatPage.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useChat } from '../../components/context/chatContext';
+import SideDrawer from '../../components/sideDrawer/SideDrawer';
+import MyChat from '../../components/myChat/MyChat';
+import { Box } from "@chakra-ui/react";
 const ChatPage = () => {
     const [chat, setChat] = useState([]);
     const {user} = useChat();
+    const token = localStorage.getItem("token");
     useEffect(()=> {
     (async ()=> {
         try{
@@ -14,10 +18,13 @@ const ChatPage = () => {
             console.log(error);
         }
     })()
-    }, []);
+    }, []); 
   return (
-    <div>
-
+    <div style={{width: "100%"}}>
+       {token && <SideDrawer/> }
+      <Box d="flex" justify-content='space-between' w='100%' h='91.5vh' p='10px'>
+        {token && <MyChat/>}
+      </Box>
     </div>
   )
 }
